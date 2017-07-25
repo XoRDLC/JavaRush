e98dfe55a8fb1224b2c0e0bdbff213d0d5697c5a
XoRDLC <deadlords@mail.ru>
2017-07-25 21:34:52 +0300

    Date / Time / Calendar learning

---
+++ x.SelfTasks/src/examplesCheck/L002_TimeDateCalendar.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2017. Код создан Д.Кляусом. Для использования кода в коммерческих продуктах - свяжитесь @ : deadlords@mail.ru
+ */
+
+
+package examplesCheck;
+
+
+import java.sql.Time;
+import java.util.Date;
+import java.util.Calendar;
+import java.util.Locale;
+
+public class L002_TimeDateCalendar {
+    public static void main(String[] args) {
+        new L002_TimeDateCalendar().new TimeC().setPTime();
+
+
+
+    }
+
+    class TimeC {
+        private Date time;
+
+        public void setPTime(){
+            System.out.println(Calendar.getInstance().getClass());
+            for(String element: Calendar.getAvailableCalendarTypes()){
+                System.out.println(element);
+            }
+
+            for(Locale element: Calendar.getAvailableLocales()){
+                System.out.printf("%s\t", element.getCountry());
+                //System.out.printf("%s\t", element.getDisplayCountry());
+                //System.out.printf("%s\t", element.getDisplayName());
+                System.out.printf("%s\t", element.getDisplayScript());
+                System.out.println();
+            }
+        }
+    }
+}
