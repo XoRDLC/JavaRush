80be5eba46b5bb07316b2eca4ebc2390214fa99c
XoRDLC <deadlords@mail.ru>
2017-07-31 22:07:15 +0300

    Прямоугольники в матрице

--- 2.JavaCore/src/com/javarush/task/task20/task2026/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task20/task2026/Solution.java
@@ -15,7 +15,31 @@ public class Solution {
         System.out.println("count = " + count + ". Должно быть 2");
     }
 
+    //какое-то топорное решение, повтор условий, брутфорс.. брр
+    //вернуться при наличии времени
+
+
     public static int getRectangleCount(byte[][] a) {
-        return 0;
+        int recCount =0;
+        for(int i=0; i<a.length;i++){
+            for(int j=0; j<a[i].length; j++){
+                if(a[i][j]==1){
+                    if(i==0&&j==0){
+                        recCount++;
+                    }
+                    else if(i==0&&j>0){
+                        if(a[i][j-1]==0)recCount++;
+
+                    }
+                    else if(j==0&&i>0){
+                        if(a[i-1][j]==0)recCount++;
+                    }
+                    else if(a[i-1][j]==0&&a[i][j-1]==0&&a[i-1][j-1]==0) recCount++;
+                }
+            }
+        }
+
+
+        return recCount;
     }
 }
